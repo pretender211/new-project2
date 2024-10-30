520615031cb4a036739d6027787bbe9d67679334
Pretender <bejbarysajdyn149@gmail.com>
2024-10-30 22:37:21 +0600

    i do hw today 30.10.24

---
+++ hw.30.10.js
@@ -0,0 +1,11 @@
+var modelname = "Chevrolet Corvette Stingray"
+var color = "red"
+var wheelsCount = 5
+var isOffRoad = "no"
+var rating = 9
+
+console.log("modelname", modelname);
+console.log("color", color);
+console.log("wheelsCount", wheelsCount);
+console.log("isOffRoad", isOffRoad);
+console.log("rating", rating);
