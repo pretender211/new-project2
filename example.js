75b482f09bcc96f890884c49dc18d8bb05355d60
Pretender <bejbarysajdyn149@gmail.com>
2024-10-30 10:49:26 +0600

    Example of variables

---
+++ example.js
@@ -0,0 +1,14 @@
+var name = "Nazar"//auspaly
+
+name =  "Abumansur"
+
+name = "Syrym"
+
+console.log(name);
+
+const phone = "192.168.123.10"//turakty
+
+phone = "192.157.35.10"
+
+console.log(phone);
+
