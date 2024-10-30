f915742ae44b8ba18676ebdc833963bca903a10a
Pretender <bejbarysajdyn149@gmail.com>
2024-10-30 22:38:34 +0600

    i write new code by js

---
+++ aboutme.js
@@ -0,0 +1,14 @@
+var name = "Beibarys"
+var surname = "Aidyn"
+var age = 16
+var weight = 64 // salmak
+var height = 184 // boi
+var school = 47 // mektep 
+
+
+console.log("name", name);
+console.log("surname", surname);
+console.log("age", age);
+console.log("weight", weight);
+console.log("height", height);
+console.log("school", school);
---
+++ test.js
@@ -0,0 +1,11 @@
+var streetName = "Maulenov, 92"; //string
+var currentday = 30; // number
+var amirkeldi = false // booten true = true, false = false
+var janasumka = null; // bos man
+var car = undefined; // undefined
+
+console.log("street", streetName);
+console.log("currentday", currentday);
+console.log("amirkeldi", amirkeldi);
+console.log("janasumka zattar", janasumka)
+console.log("car bar", car);
