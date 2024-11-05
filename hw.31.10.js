656a9e44dea5f9f9216d5d25d5648dd4a6107a8c
Pretender <bejbarysajdyn149@gmail.com>
2024-11-05 18:36:22 +0600

    i do hw.31.10

---
+++ hw.31.10.js
@@ -0,0 +1,13 @@
+var slicesPerPizza = 8;
+var numberOfPizza = 5;
+let totalSlices = slicesPerPizza * numberOfPizza;
+console.log(totalSlices)
+
+var totalChocolates = 24;
+var numberOfFriends = 6;
+let chocolatesPerFriends = totalChocolates / numberOfFriends;
+console.log(chocolatesPerFriends)
+
+var plantHeights = 10;
+plantHeights += 2;
+console.log(plantHeights)
